405f8b091596cc44d67717755619471b1b6b3c06
shady-chen <710254315@qq.com>
2017-12-31 09:49:28 +0800

    123

--- shop_demo/Common/Conf/config.php
+++ shop_demo/Common/Conf/config.php
@@ -14,10 +14,10 @@ return array(
  
 	// 数据库设置
     'DB_TYPE'               =>  'mysql',     // 数据库类型
-    'DB_HOST'               =>  'localhost', // 服务器地址
+    'DB_HOST'               =>  '47.95.221.25', // 服务器地址
     'DB_NAME'               =>  'cntx',          // 数据库名
     'DB_USER'               =>  'root',      // 用户名
-    'DB_PWD'                =>  'root',          // 密码
+    'DB_PWD'                =>  'password',          // 密码
     'DB_PORT'               =>  '',        // 端口  默认为3306
     'DB_PREFIX'             =>  '',    // 数据库表前缀
     'DB_PARAMS'          	=>  array(), // 数据库连接参数    
